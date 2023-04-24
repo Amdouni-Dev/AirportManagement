8dd11e58c9c4ae855ca0cd884ed2d899075ea68c
Yassmin Mouaddeb <98842040+ElMeddebYassmin@users.noreply.github.com>
2023-04-24 22:19:58 +0100

    PATRON DE CONCEPTION

---
+++ AM.Core.Services/PlaneService.cs
@@ -0,0 +1,35 @@
+﻿using AM.Core.Domain;
+using AM.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Core.Services
+{
+    public class PlaneService : IPlaneService
+    {
+        IRepository<Plane> repository;
+        public PlaneService(IRepository<Plane> repository)
+        {
+            this.repository = repository;
+        }
+        public void Add(Plane plane)
+        {
+            repository.Add(plane);
+            repository.Commit();
+        }
+
+        public void Delete(Plane plane)
+        {
+            repository.Delete(plane);
+            repository.Commit();
+        }
+
+        public IList<Plane> GetAll()
+        {
+            return repository.GetAll();
+        }
+    }
+}
